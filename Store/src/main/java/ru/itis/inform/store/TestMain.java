45f0908c9d50f321a6cf602976c010eccc38dc3f
Salavat Zabirov <zab.sal42@gmail.com>
2016-03-12 00:10:38 +0300

    TestMain check is fixed

--- Store/src/main/java/ru/itis/inform/store/TestMain.java
+++ Store/src/main/java/ru/itis/inform/store/TestMain.java
@@ -1,24 +1,17 @@
 package ru.itis.inform.store;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.itis.inform.store.dao.ItemsDao;
-import ru.itis.inform.store.dao.models.Item;
 import ru.itis.inform.store.services.StoreService;
 
 public class TestMain {
 
-    @Autowired
-    @Qualifier("itemsDaoTsv")
     static StoreService storeService;
 
     public static void main(String[] args) {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(StoreConfiguration.class);
         storeService = (StoreService) context.getBean("storeService");
-        System.out.println(storeService == null);
+        storeService.isExist("milk");
     }
 }
